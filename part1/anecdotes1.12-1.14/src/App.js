dd79e9ad0a86f57c2b7e16c1a803b336a0500fd6
joshuazhou744 <128266380+joshuazhou744@users.noreply.github.com>
2023-05-13 19:33:46 -0600

    Finish anecdotes

---
+++ part1/anecdotes1.12-1.14/src/App.js
@@ -0,0 +1,62 @@
+import { useState } from 'react'
+
+const App = () => {
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
+
+  const [votes, setVotes] = useState(new Uint8Array(anecdotes.length))
+  const handleVote = () => {
+    const copy = [...votes]
+    copy[selected]++
+    setVotes(copy)
+  }
+  let max = Math.max(...votes)
+   
+  const [selected, setSelected] = useState(0)
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+  }
+
+  const indexWithMostVotes = votes.reduce((iMax, x, i, arr) => x > arr[iMax] ? i : iMax, 0);
+
+  return (
+    <div>
+      <Title text='Anecdote of the day' />
+      {anecdotes[selected]} 
+      <br></br>
+      has {votes[selected]} votes
+      <br></br>
+      <Button text='next anecdote'handleClick={() => setSelected(getRandomInt(anecdotes.length))}/>
+      <Button text='vote'handleClick={() => handleVote()}/>
+      <Title text='Anecdote with most votes' />
+      {anecdotes[indexWithMostVotes]}
+      <br></br>
+      has {max} votes
+      <br></br>
+    </div>
+  )
+}
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>
+      {props.text}
+    </button>
+  )
+}
+
+const Title = (props) => {
+  return (
+    <h1>{props.text}</h1>
+  )
+}
+
+export default App
