df62a20b0a15c039371accb94468c6469c4d3f5f
joshuazhou744 <128266380+joshuazhou744@users.noreply.github.com>
2023-06-18 11:06:01 -0600

    2.11

--- part2/2.6-2.11/phonebook/src/components/Filter.js
+++
@@ -1,9 +0,0 @@
-const Filter = ({ query, handleChange }) => {
-    return(
-      <div>
-        search: <input value={query} onChange={handleChange}/>
-      </div>
-    )
-}
-
-export default Filter
