f8439b1271269de54d4e82df44514b27a961f239
joshuazhou744 <128266380+joshuazhou744@users.noreply.github.com>
2023-08-04 10:04:32 -0600

    3.5

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": 1,
@@ -46,6 +48,33 @@ app.get('/api/persons/:id', (request, response) => {
     } else {
         response.status(404).end()
     }
+})
+
+const generateId = () => {
+    const maxId = persons.length > 0
+        ? Math.max(...persons.map(p => p.id))
+        : 0
+    return maxId + 1
+}
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    const id = generateId()
+
+    if (!body.name) {
+        return response.status(400).json({
+            error: "name or number is missing"
+        })
+    }
+
+    const person = {
+        id,
+        name: body.name,
+        number: body.number
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
 })
 
 app.delete('/api/persons/:id', (request, response) => {
